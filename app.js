6c4786fbaa038da4bf13f84f13ecd9919d4c7bcb
tcgj <terencechong@Terence-Chong-zhangguangjun.local>
2019-01-20 03:48:10 +0800

    Set up account authentication

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongo = require('mongodb');
-var monk = require('monk');
-var db = monk('localhost:27017/foodwasteproj');
+var mongoose = require('mongoose');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
@@ -16,20 +17,31 @@ var app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
 
+//app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(require('express-session')({
+      secret: 'keyboard cat',
+      resave: false,
+      saveUninitialized: false
+}));
+app.use(passport.initialize());
+app.use(passport.session());
 app.use(express.static(path.join(__dirname, 'public')));
-
-// db access
-app.use(function(req, res, next) {
-  req.db = db;
-  next();
-});
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+
+// passport config
+var Account = require('./models/account');
+passport.use(new LocalStrategy(Account.authenticate()));
+passport.serializeUser(Account.serializeUser());
+passport.deserializeUser(Account.deserializeUser());
+
+// mongoose
+mongoose.connect('mongodb://localhost/passport');
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
